6eb9d190c33e988c8f278e1d0eec8898b082c885
Hubert Lipiński <hubertlipinskipl@gmail.com>
2025-06-05 15:57:05 +0200

    chore: refactor

--- server/models/User.ts
+++ server/models/User.ts
@@ -15,31 +15,3 @@ export interface UserDocument {
   createdAt: Date
   updatedAt: Date
 }
-
-// const userConsentSchema = new Schema({
-//   key: {
-//     type: String,
-//     required: true,
-//   },
-//   value: {
-//     type: Boolean,
-//     required: true,
-//     default: false,
-//   },
-// }, { _id: false })
-
-// const UserSchema = new Schema({
-//   name: { type: String, required: true, unique: false, trim: true, lowercase: false },
-//   username: { type: String, required: true, unique: false, trim: true, lowercase: true },
-//   email: { type: String, unique: true, required: true, lowercase: true, trim: true },
-//   password: { type: String, required: true, trim: true, length: [8, 'Password must be at least 8 characters'] },
-//   consent: {
-//     type: [userConsentSchema],
-//     default: [
-//       { key: 'agreement', value: true },
-//       { key: 'marketing', value: false },
-//     ],
-//   },
-// }, { timestamps: true, versionKey: false })
-
-// export const User = model<UserDocument>('User', UserSchema)
